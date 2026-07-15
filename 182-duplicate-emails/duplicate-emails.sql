8137a34a4f640a72422872034343c508a5e37bc5
Biswajit Sahoo <154993673+biswajitt2006@users.noreply.github.com>
2026-07-15 10:22:07 +0530

    Time: 395 ms (67.46%) | Memory: 0B (100.00%) - LeetSync

---
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+SELECT email as Email 
+FROM Person 
+GROUP BY email
+HAVING COUNT(email) >1 
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
