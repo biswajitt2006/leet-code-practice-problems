dc6269f081b82fa3a75bc2a3bde6488425da6ba3
Biswajit Sahoo <154993673+biswajitt2006@users.noreply.github.com>
2026-07-19 10:24:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (51.91%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,14 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
         dp=[-1]*(n+1)
-        def solve(n):
-            if n <= 2 :
-                return n
-            if dp[n] != -1 :
-                return dp[n]
-            dp[n]=solve(n-1)+solve(n-2)
-            return dp[n]
-        return solve(n)
+        dp[0]=0
+        dp[1]=1
+        dp[2]=2
+        for i in range(3,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
         
 
 # Synced seamlessly with LeetHub Pro
