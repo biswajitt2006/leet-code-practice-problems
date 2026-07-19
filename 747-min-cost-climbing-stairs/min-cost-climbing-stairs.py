41ead85ca81ff8e85f71a1a7a896187daabd01b9
Biswajit Sahoo <154993673+biswajitt2006@users.noreply.github.com>
2026-07-19 12:50:01 +0530

    Time: 6 ms (17.65%) | Memory: 19.4 MB (25.10%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -7,13 +7,10 @@ class Solution:
             if n <= 1:
                 return 0
 
-            if dp[n] != -1:
-                return dp[n]
-
-            dp[n] = min(
-                minCost(n - 1) + cost[n - 1],
-                minCost(n - 2) + cost[n - 2]
-            )
+            dp[0]=0
+            dp[1]=0
+            for i in range(2,n+1):
+                dp[i]=min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2])
 
             return dp[n]
 
